80bad29b5312a8c9f4999425ea970016e16f72ca
hokwach <hezronokwach@gmail.com>
2024-08-20 12:25:00 +0300

    Add handler for the locationHandler Start server at port 3000

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
 	api "groupie/handlers"
 )
 
-func main(){
-	
-	fmt.Println(api.ReadApi())
+func main() {
+	http.HandleFunc("/locations", api.LocationHandler)
+	http.ListenAndServe(":3000", nil)
 }
