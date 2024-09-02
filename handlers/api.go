3f5b262d93a41d7172e2bee66db7ba0716e6accc
hokwach <hezronokwach@gmail.com>
2024-09-02 15:37:46 +0300

    Add handlers and api functions

--- handlers/api.go
+++ handlers/api.go
@@ -4,34 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"text/template"
 )
-
-type Location struct {
-	ID        int64    `json:"id"`
-	Locations []string `json:"locations"`
-}
-
-func LocationHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Wrong method", http.StatusMethodNotAllowed)
-		return 
-	}
-
-	temp1, err := template.ParseFiles("template/index.html")
-	if err != nil {
-		http.Error(w, "Error loading template", http.StatusInternalServerError)
-		return
-	}
-
-	Result := ReadApi()
-
-	// Pass the result to the template
-	err = temp1.Execute(w, Result)
-	if err != nil {
-		http.Error(w, "Error executing template", http.StatusInternalServerError)
-	}
-}
 
 func ReadApi() Location {
 	url := "https://groupietrackers.herokuapp.com/api/locations/1"
---
+++ handlers/structs.go
@@ -0,0 +1,18 @@
+package api
+
+type Location struct {
+	ID        int64    `json:"id"`
+	Locations []string `json:"locations"`
+}
+
+type Artist struct {
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
+}
