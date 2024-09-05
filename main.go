0e153a7a25d8f78823b3006d2746a73fd60556a0
hanapiko <apikojuma94@gmail.com>
2024-09-05 10:42:37 +0300

    fix: Remove dateHandler

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ func main() {
 	http.HandleFunc("/locations/", api.LocationHandler)
 	http.HandleFunc("/", api.ArtistsHandler)
 	http.HandleFunc("/artist/", api.ArtistHandler)
-	http.HandleFunc("/dates/", api.DateHandler)
 
 	fmt.Println("server running")
 	http.ListenAndServe(":3000", nil)
