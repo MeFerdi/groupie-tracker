c75a749af32f325f9e6e167d211dd155048a2b94
hanapiko <apikojuma94@gmail.com>
2024-09-05 11:09:47 +0300

    feat:Implement DateHandler for serving date-related templates

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -69,7 +69,7 @@ func ArtistHandler(w http.ResponseWriter, r *http.Request) {
 func LocationHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodGet {
 		http.Error(w, "Wrong method", http.StatusMethodNotAllowed)
-		return 
+		return
 	}
 	id1 := strings.Split(r.URL.Path, "/")
 	if len(id1) < 3 {
@@ -78,14 +78,13 @@ func LocationHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	id := id1[len(id1)-1]
 
-
 	temp1, err := template.ParseFiles("template/index.html")
 	if err != nil {
 		http.Error(w, "Error loading template", http.StatusInternalServerError)
 		return
 	}
 
-	Result,_ := ReadLocation(id)
+	Result, _ := ReadLocation(id)
 
 	// Pass the result to the template
 	err = temp1.Execute(w, Result)
@@ -94,4 +93,27 @@ func LocationHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func DateHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Wrong method", http.StatusMethodNotAllowed)
+		return
+	}
+	id1 := strings.Split(r.URL.Path, "/")
+	if len(id1) < 3 {
+		http.Error(w, "Artist ID not found", http.StatusBadRequest)
+		return
+	}
+	id := id1[len(id1)-1]
 
+	temp1, err := template.ParseFiles("template/dates.html")
+	if err != nil {
+		http.Error(w, "Error loading template", http.StatusInternalServerError)
+		return
+	}
+
+	Result, _ := ReadDate(id)
+	err = temp1.Execute(w, Result)
+	if err != nil {
+		http.Error(w, "Error executing template", http.StatusInternalServerError)
+	}
+}
