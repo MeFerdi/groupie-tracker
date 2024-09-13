154af362a7abbf1cd8d121274489317a30bb1d9c
MeFerdi <ferdinandodhis254@gmail.com>
2024-09-13 23:11:47 +0300

    Chore: implement home page redirecting to artists page

--- handlers/readrelation.go
+++ handlers/readrelation.go
@@ -6,14 +6,14 @@ import (
 )
 
 func FetchRelations(id string) (Relation, error) {
-	// Make a GET request to the API endpoint for relation data
+	
 	res, err := http.Get("https://groupietrackers.herokuapp.com/api/relation/" + id)
 	if err != nil {
 		return Relation{}, err
 	}
-	defer res.Body.Close() // Ensure the response body is closed after the function returns
+	defer res.Body.Close()
 
-	// Decode the JSON response into a RelationData struct
+	
 	var data Relation
 	if err := json.NewDecoder(res.Body).Decode(&data); err != nil {
 		return Relation{}, err
